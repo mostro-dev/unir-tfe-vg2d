2f99667e3a571d28d3f36985e2f65155e6b18726
Freddy Orozco <freddycamilo95@gmail.com>
2025-05-24 12:36:27 -0500

    feat: Add python version + config + requirements

---
+++ config.py
@@ -0,0 +1,10 @@
+# VisualBoyAdvance window region config (macOS)
+y_offset = 55
+x_offset = 64
+window_width = 480
+window_height = 370
+
+calc_window_width = window_width - (x_offset * 2)
+calc_window_height = window_height - y_offset
+
+GAME_REGION = (x_offset, y_offset, calc_window_width, calc_window_height)
