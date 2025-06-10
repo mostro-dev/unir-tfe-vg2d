955a48a706de4a46e3c99131304f9d16a0b12658
Freddy Orozco <freddycamilo95@gmail.com>
2025-06-09 20:26:36 -0500

    feat: Update the move and press methods from keyboard controller

--- game_agent/controller/keyboard_controller.py
+++ game_agent/controller/keyboard_controller.py
@@ -1,12 +1,18 @@
 import pyautogui
 import time
 
-def press(key, duration=0.2):
+
+def press(key, duration=0.1):
     pyautogui.keyDown(key)
     time.sleep(duration)
     pyautogui.keyUp(key)
     time.sleep(0.1)
 
+
 def move(direction, steps=1):
-    for _ in range(steps):
-        press(direction)
+    time.sleep(0.1)
+    print("Interacting with A Button")
+    press(direction)
+    time.sleep(0.1)
+    # for _ in range(steps):
+    #     press(direction)
