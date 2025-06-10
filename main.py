3aa9c34b7e742941372f2dc924d6475fd4eca0fc
Freddy Orozco <freddycamilo95@gmail.com>
2025-06-10 18:41:21 -0500

    feat: Fix text blocker

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from game_agent.controller.keyboard_controller import move, press
 from game_agent.dqn.environment import GameEnvironment
 from config import GAME_REGION
+from game_agent.vision.dialog_detector import is_dialog_open_by_template
 from game_agent.vision.screen_reader import capture_region, save_image
 from game_agent.vision.tile_utils import get_surrounding_obstacles, split_into_tiles
 from game_agent.vision.transform_pipeline import save_image_pipeline
@@ -42,9 +43,16 @@ time.sleep(3)
 # print("Estado:", state)
 
 # Inicializar entorno
+
 game_env = GameEnvironment()
 player_top_left = game_env.player_pos
 
+frame = capture_region(GAME_REGION)
+
+if is_dialog_open_by_template(frame):
+    print("💬 Cuadro de texto detectado. Presionando Z.")
+    press('z')
+    time.sleep(0.5)
 
 def images_different(img1, img2, threshold=0.05):
     diff = np.abs(img1.astype(np.float32) - img2.astype(np.float32)) / 255.0
@@ -66,6 +74,11 @@ def is_real_obstacle(direction, threshold=0.05, debug=True):
 
     press('z')
     time.sleep(1)
+    
+    while is_dialog_open_by_template(capture_region(GAME_REGION)):
+        print("💬 Cuadro de texto detectado. Presionando Z.")
+        press('z')
+        time.sleep(0.5)
 
     move(direction)
     time.sleep(1)
@@ -80,6 +93,8 @@ def is_real_obstacle(direction, threshold=0.05, debug=True):
     if debug:
         print(f"[DEBUG] Obstáculo real en dirección '{direction}'.")
     return True
+
+
 
 
 # Loop de movimiento tipo serpiente
